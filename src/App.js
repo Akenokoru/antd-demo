e4b48dee636a09c33d28fb9cc2cb64b5d7d6aaec
Akenokoru <akenokoru.pan@gmail.com>
2020-03-11 14:34:18 +0800

    Add less less-loader.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button } from "antd";
-import "./App.css";
+import "./App.less";
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
 
 export default App;
